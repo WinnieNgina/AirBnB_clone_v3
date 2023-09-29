a9041ca789f68d0d7a189278570dcec0060a2188
Winfred <winfrednginakilonzo@gmail.com>
2023-09-29 23:06:43 +0300

    Curl status code

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-from flask import Flask, render_template, Blueprint
+"""Main file"""
+
+import os
+from flask import Flask
 from models import storage
 from api.v1.views import app_views
-import os
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -16,7 +18,7 @@ def teardown_db(exception):
 
 if __name__ == '__main__':
     host = os.getenv('HBNB_API_HOST', '0.0.0.0')
-    port = int(os.getenv('HBNB_API_PORT', '5000'))
+    port = int(os.getenv('HBNB_API_PORT', 5000))
     # getenv returns a string and port is an int
     # THREADED is set to true so it can serve multiple requests at once
     app.run(host=host, port=port, threaded=True)
