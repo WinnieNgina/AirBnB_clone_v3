15d96de9032017df2ee461a70c6dc89f3ca899b0
Winfred <winfrednginakilonzo@gmail.com>
2023-09-29 22:33:37 +0300

    Create Blueprint

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+from flask import Flask, render_template, Blueprint
+from models import storage
+from api.v1.views import app_views
+import os
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """closes the storage on teardown"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    host = os.getenv('HBNB_API_HOST', '0.0.0.0')
+    port = int(os.getenv('HBNB_API_PORT', '5000'))
+    # getenv returns a string and port is an int
+    # THREADED is set to true so it can serve multiple requests at once
+    app.run(host=host, port=port, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from flask import Flask, jsonify
+from api.v1.views import app_views
+
+
+@app_views.route('/status')
+def status_check():
+    '''Returns status code'''
+    return jsonify({"status": "OK"})
