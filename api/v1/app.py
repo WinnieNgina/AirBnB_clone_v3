0f962180b85a4834a73632ca17b670541ae30c2d
Winfred <winfrednginakilonzo@gmail.com>
2023-09-30 00:42:43 +0300

    Fix code

--- api/v1/app.py
+++ api/v1/app.py
@@ -19,7 +19,7 @@ def teardown_db(exception):
 @app.errorhandler(404)
 def page_not_found(error):
     '''Handle not found error'''
-    return jsonify({"error": "Not found"})
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == '__main__':
